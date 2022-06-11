98755aa97139efbf1c6cff286ac70dc8f39079bf
Liberbell <liberbell@gmail.com>
2022-06-11 15:22:02 +0900

    schema4

--- mysqlschema.sql
+++ mysqlschema.sql
@@ -4,4 +4,8 @@ USE `company`;
 
 CREATE TABLE IF NOT EXISTS `employee` (
     `id` INT unsigned NOT NULL AUTO_INCREMENT,
-)
+    `name` VARCHAR(20),
+    `city` VARCHAR(20) DEFAULT "fredericton",
+    `dept` VARCHAR(20) DEFAULT "onboarding",
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CARSET=utf8mb4;
