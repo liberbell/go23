7ea3e7d569bb55240fb3d1da5b7e60e22298ccee
Liberbell <liberbell@gmail.com>
2022-06-12 18:50:36 +0900

    mysqlschema1

--- mysqlschema.sql
+++ mysqlschema.sql
@@ -2,10 +2,10 @@ CREATE DATABASE `company`;
 
 USE `company`;
 
-CREATE TABLE IF NOT EXISTS `employee` (
+CREATE TABLE IF NOT EXISTS `employees` (
     `id` INT unsigned NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20),
     `city` VARCHAR(20) DEFAULT "fredericton",
     `dept` VARCHAR(20) DEFAULT "onboarding",
     PRIMARY KEY(`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4;
