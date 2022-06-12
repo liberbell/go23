48491a0ab1c89e8200344c553fc1623af76d6f04
Liberbell <liberbell@gmail.com>
2022-06-12 18:54:07 +0900

    employees2

--- employeesdata.sql
+++ employeesdata.sql
@@ -1,3 +1,15 @@
 USE `company`;
 
 INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("joe", "fredericton", "sales");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("jill", "boston", "development");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("alex", "tronto", "development");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("anna", "new york", "development");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("ben", "huston", "sales");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("eve", "tampa", "operations");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("elliot", "vancouver", "operations");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("grace", "london", "sales");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("simon", "bangalore", "development");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("tara", "dublin", "sales");
+INSERT INTO `company`.`employees` (`name`,`city`,`dept`) VALUES ("tim", "new york", "development");
+
+SELECT * FROM `employees`;
