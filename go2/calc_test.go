b8459e3129c7235fcbf887c4e38facd4578fcbdf
Liberbell <liberbell@gmail.com>
2022-06-14 08:56:24 +0900

    calctest2

--- go2/calc_test.go
+++ go2/calc_test.go
@@ -1,10 +1,14 @@
 package calc_test
 
-func TestAdd(t *testing.T)  {
-	got := calc.(2, 1)
+func TestAdd(t *testing.T) {
+	got := calc.Add(2, 1)
 	expected := 3
 
 	if got != expected {
 		t.Errorf("not expected result: %v expected: %v", got, expected)
 	}
+}
+
+func TestSub(t *testing.T) {
+
 }
