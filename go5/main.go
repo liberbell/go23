6ec14571bb7c3ff7ee760519a6e9cf1e64d0e36d
Liberbell <liberbell@gmail.com>
2022-06-16 08:57:13 +0900

    main3

--- go5/main.go
+++ go5/main.go
@@ -9,7 +9,8 @@ const (
 	Password = "password"
 )
 
-var big int = 9223372036854775807
+// var big int = 9223372036854775807 + 1
+const big = 9223372036854775807 + 1
 
 func main() {
 	fmt.Println(pi, Username, Password)
