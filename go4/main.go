f0c4511768e47b9812eb11494c89d35704e4cb79
Liberbell <liberbell@gmail.com>
2022-06-15 09:19:03 +0900

    main6

--- go4/main.go
+++ go4/main.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// var (
+	// 	i    int     = 1
+	// 	f64  float64 = 1.2
+	// 	s    string  = "test"
+	// 	t, f bool    = true, false
+	// )
+
 	var (
-		i    int     = 1
-		f64  float64 = 1.2
-		s    string  = "test"
-		t, f bool    = true, false
+		i    int
+		f64  float64
+		s    string
+		t, f bool
 	)
 
 	// var f bool = false
