f3d0e7322822eefe687a6b6e64b0b669c0105aee
Liberbell <liberbell@gmail.com>
2022-06-15 09:14:34 +0900

    main2

--- go4/main.go
+++ go4/main.go
@@ -6,8 +6,8 @@ func main() {
 	var i int = 1
 	var f64 float64 = 1.2
 	var s string = "test"
-	var t bool = true
-	var f bool = false
+	var t, f bool = true, false
+	// var f bool = false
 
 	fmt.Println(i, f64, s, t, f)
 }
