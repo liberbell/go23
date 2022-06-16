3641e9448974f20f344f62851328e8baee111851
Liberbell <liberbell@gmail.com>
2022-06-17 08:40:49 +0900

    main2

--- go6/main.go
+++ go6/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		u8  uint8     = 255
-		i8  int8      = 127
-		f32 float32   = 0.2
-		c64 complex64 = -5 + 12i
-	)
-	fmt.Println(u8, i8, f32, c64)
+	// var (
+	// 	u8  uint8     = 255
+	// 	i8  int8      = 127
+	// 	f32 float32   = 0.2
+	// 	c64 complex64 = -5 + 12i
+	// )
+	// fmt.Println(u8, i8, f32, c64)
 
-	fmt.Printf("Type=%T, Value=%v\n", u8, u8)
+	// fmt.Printf("Type=%T, Value=%v\n", u8, u8)
+
+	fmt.Println("1 + 1 = ", 1+1)
 }
