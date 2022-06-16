2f338256196ccbfaa0679ea7ee4e8c6d306046db
Liberbell <liberbell@gmail.com>
2022-06-17 08:46:29 +0900

    main5

--- go6/main.go
+++ go6/main.go
@@ -13,12 +13,17 @@ func main() {
 
 	// fmt.Printf("Type=%T, Value=%v\n", u8, u8)
 
-	fmt.Println("1 + 1 = ", 1+1)
-	fmt.Println("10 - 1", 10-1)
-	fmt.Println("10 / 2 = ", 10/2)
-	fmt.Println("10 / 3 = ", 10/3)
-	fmt.Println("10.0 / 3 = ", 10.0/3)
-	fmt.Println("10 / 3.0 = ", 10/3.0)
-	fmt.Println("10 % 2 = ", 10%2)
-	fmt.Println("10 % 3 = ", 10%3)
+	// fmt.Println("1 + 1 = ", 1+1)
+	// fmt.Println("10 - 1", 10-1)
+	// fmt.Println("10 / 2 = ", 10/2)
+	// fmt.Println("10 / 3 = ", 10/3)
+	// fmt.Println("10.0 / 3 = ", 10.0/3)
+	// fmt.Println("10 / 3.0 = ", 10/3.0)
+	// fmt.Println("10 % 2 = ", 10%2)
+	// fmt.Println("10 % 3 = ", 10%3)
+
+	x := 0
+	fmt.Println(x)
+	x++
+	fmt.Println(x)
 }
