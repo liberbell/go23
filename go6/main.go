663335355fd55e9eed8a90856e2875e4d9ac1589
Liberbell <liberbell@gmail.com>
2022-06-17 08:43:13 +0900

    main4

--- go6/main.go
+++ go6/main.go
@@ -19,4 +19,6 @@ func main() {
 	fmt.Println("10 / 3 = ", 10/3)
 	fmt.Println("10.0 / 3 = ", 10.0/3)
 	fmt.Println("10 / 3.0 = ", 10/3.0)
+	fmt.Println("10 % 2 = ", 10%2)
+	fmt.Println("10 % 3 = ", 10%3)
 }
