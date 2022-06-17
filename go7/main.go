e12b29f56798d2d5c8a8cf16c629a949d4e271ad
Liberbell <liberbell@gmail.com>
2022-06-17 09:03:34 +0900

    main7

--- go7/main.go
+++ go7/main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World.")
-	fmt.Println("Hello " + "World.")
-	fmt.Println(string("Hello World."[0]))
+	// 	fmt.Println("Hello World.")
+	// 	fmt.Println("Hello " + "World.")
+	// 	fmt.Println(string("Hello World."[0]))
 
-	var s string = "Hello World."
+	// 	var s string = "Hello World."
 
-	fmt.Println(strings.Replace(s, "H", "A", 1))
-	fmt.Println(strings.Contains(s, "World"))
+	// 	fmt.Println(strings.Replace(s, "H", "A", 1))
+	// 	fmt.Println(strings.Contains(s, "World"))
 
-	fmt.Println(`Test
-Test`)
-	fmt.Println("\"")
-	fmt.Println(`"`)
+	// 	fmt.Println(`Test
+	// Test`)
+	// 	fmt.Println("\"")
+	// 	fmt.Println(`"`)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
 }
