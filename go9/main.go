de19f0915d36854eb280e0c3dfeba174e4682e03
Liberbell <liberbell@gmail.com>
2022-06-18 15:11:57 +0900

    main4

--- go9/main.go
+++ go9/main.go
@@ -3,17 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
+	// var a [2]int
+	// a[0] = 100
+	// a[1] = 200
 
-	fmt.Println(a)
+	// fmt.Println(a)
 
-	var b [2]int = [2]int{100, 200}
+	// var b [2]int = [2]int{100, 200}
 
-	fmt.Println(b)
+	// fmt.Println(b)
 
-	var c []int = []int{100, 200}
-	c = append(c, 300)
-	fmt.Println(c)
+	// var c []int = []int{100, 200}
+	// c = append(c, 300)
+	// fmt.Println(c)
+
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
 }
