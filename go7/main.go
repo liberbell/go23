4b526c420cb634e090d39f98c71ae10e6facc8c6
Liberbell <liberbell@gmail.com>
2022-06-18 14:49:29 +0900

    main2

--- go7/main.go
+++ go7/main.go
@@ -23,6 +23,6 @@ func main() {
 	// fmt.Println(t, f)
 	t, f := true, false
 	fmt.Println(t, f)
-	fmt.Printf("%T %v\n", t, t)
-	fmt.Printf("%T %v\n", f, f)
+	fmt.Printf("%T %v %t\n", t, t, t)
+	fmt.Printf("%T %v %t\n", f, f, 0)
 }
