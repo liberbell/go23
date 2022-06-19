dba2a1c067cfb71a133c96cbbf95bea1368607ec
Liberbell <liberbell@gmail.com>
2022-06-20 08:45:10 +0900

    main5

--- go12/main.go
+++ go12/main.go
@@ -8,7 +8,14 @@ func add(x int, y int) (int, int) {
 	return x + y, x - y
 }
 
+func cal(price, item int) (result int) {
+	result = price * item
+	return result
+}
+
 func main() {
 	r1, r2 := add(10, 20)
 	fmt.Println(r1, r2)
+
+	cal(100, 9)
 }
