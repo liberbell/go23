88d0afb4acce7509255841bc1872a0a0cd38adab
Liberbell <liberbell@gmail.com>
2022-06-19 18:14:59 +0900

    main4

--- go9/main.go
+++ go9/main.go
@@ -17,24 +17,27 @@ func main() {
 	// c = append(c, 300)
 	// fmt.Println(c)
 
-	n := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(n)
-	fmt.Println(n[2])
-	fmt.Println(n[2:4])
-	fmt.Println(n[:2])
-	fmt.Println(n[2:])
-	fmt.Println(n[:])
+	// n := []int{1, 2, 3, 4, 5, 6}
+	// fmt.Println(n)
+	// fmt.Println(n[2])
+	// fmt.Println(n[2:4])
+	// fmt.Println(n[:2])
+	// fmt.Println(n[2:])
+	// fmt.Println(n[:])
 
-	n[2] = 100
-	fmt.Println(n)
+	// n[2] = 100
+	// fmt.Println(n)
 
-	var board = [][]int{
-		[]int{0, 1, 2},
-		[]int{3, 4, 5},
-		[]int{6, 7, 8},
-	}
-	fmt.Println(board)
+	// var board = [][]int{
+	// 	[]int{0, 1, 2},
+	// 	[]int{3, 4, 5},
+	// 	[]int{6, 7, 8},
+	// }
+	// fmt.Println(board)
 
-	n = append(n, 100, 200, 300, 400)
-	fmt.Println(n)
+	// n = append(n, 100, 200, 300, 400)
+	// fmt.Println(n)
+
+	n := make([]int, 3, 5)
+	fmt.Printf("len: %d, cap: %c, value: %v", n, n, n)
 }
