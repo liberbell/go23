a440184706134174324ac83bf5bd3cbec0d5cfaa
Liberbell <liberbell@gmail.com>
2022-06-19 18:15:32 +0900

    main5

--- go9/main.go
+++ go9/main.go
@@ -39,5 +39,5 @@ func main() {
 	// fmt.Println(n)
 
 	n := make([]int, 3, 5)
-	fmt.Printf("len: %d, cap: %c, value: %v", n, n, n)
+	fmt.Printf("len: %d, cap: %c, value: %v\n", len(n), cap(n), n)
 }
