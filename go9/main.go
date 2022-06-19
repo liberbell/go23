db4d1b45ee6a04e8b38b8091a887c677975fc734
Liberbell <liberbell@gmail.com>
2022-06-19 18:23:46 +0900

    main11

--- go9/main.go
+++ go9/main.go
@@ -53,7 +53,7 @@ func main() {
 	var c []int
 	fmt.Printf("len: %d, cap: %d, value: %v\n", len(c), cap(c), c)
 
-	c = make([]int, 5)
+	c = make([]int, 0, 5)
 	for i := 0; i < 5; i++ {
 		c = append(c, i)
 		fmt.Println(c)
