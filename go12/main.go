c529618728e78024f087a2694b4ef921870a7a83
Liberbell <liberbell@gmail.com>
2022-06-20 08:40:19 +0900

    main3

--- go12/main.go
+++ go12/main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-func add(x int, y int) {
+func add(x int, y int) int16 {
 	fmt.Println("Add function")
-	fmt.Println(x + y)
+	// fmt.Println(x + y)
+	return x + y
 }
 
 func main() {
-	add(10, 20)
+	result := add(10, 20)
+	fmt.Println(result)
 }
