fdebb0c5672a26f4e847d2180403b3b9df4085e6
Liberbell <liberbell@gmail.com>
2022-06-21 10:58:16 +0900

    main3

--- test1/main.go
+++ test1/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
 	f := 1.11
-	conv_f, _ := strconv.Atoi(f)
-	fmt.Println(conv_f)
+	i := int(f)
+	// conv_f, _ := strconv.Atoi(f)
+	fmt.Println(i)
 }
