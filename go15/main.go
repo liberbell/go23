46640665bd0548665fab3019d1ccb81d66cfb9ad
Liberbell <liberbell@gmail.com>
2022-06-22 08:36:34 +0900

    main7

--- go15/main.go
+++ go15/main.go
@@ -11,19 +11,22 @@ func by2(num int) string {
 }
 
 func main() {
-	num := 9
-	if num%2 == 0 {
-		fmt.Println("by 2")
-	} else if num%3 == 0 {
-		fmt.Println("by 3")
-	} else {
-		fmt.Println("else")
-	}
-	x, y := 11, 12
-	if x == 10 && y == 10 {
-		fmt.Println("&&")
-	}
-	if x == 10 || y == 10 {
-		fmt.Println("||")
-	}
+	// num := 9
+	// if num%2 == 0 {
+	// 	fmt.Println("by 2")
+	// } else if num%3 == 0 {
+	// 	fmt.Println("by 3")
+	// } else {
+	// 	fmt.Println("else")
+	// }
+	// x, y := 11, 12
+	// if x == 10 && y == 10 {
+	// 	fmt.Println("&&")
+	// }
+	// if x == 10 || y == 10 {
+	// 	fmt.Println("||")
+	// }
+
+	result := by2(10)
+	fmt.Println(result)
 }
