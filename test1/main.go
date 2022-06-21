46a1d63fdac824e0723598574c9b34410868173b
Liberbell <liberbell@gmail.com>
2022-06-21 11:00:56 +0900

    main4

--- test1/main.go
+++ test1/main.go
@@ -9,4 +9,11 @@ func main() {
 	i := int(f)
 	// conv_f, _ := strconv.Atoi(f)
 	fmt.Println(i)
+
+	m := map[string]int{
+		"Mike":  20,
+		"Nancy": 24,
+		"Messi": 30,
+	}
+	fmt.Printf("%T %v\n", m, m)
 }
