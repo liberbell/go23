18c69394318bc2e0c199da2be286da956499f45f
Liberbell <liberbell@gmail.com>
2022-06-22 08:30:59 +0900

    main3

--- go15/main.go
+++ go15/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	num := 5
+	num := 6
 	if num%2 == 0 {
 		fmt.Println("by 2")
+	} else if num%3 == 0 {
+		fmt.Println("by 3")
 	} else {
 		fmt.Println("else")
 	}
