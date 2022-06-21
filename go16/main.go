448fb3b18df9c4804a21f4e79fcb6fa579b1d351
Liberbell <liberbell@gmail.com>
2022-06-22 08:48:13 +0900

    main5

--- go16/main.go
+++ go16/main.go
@@ -3,21 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("Continue")
-			continue
-		}
-		if i > 5 {
-			fmt.Println("Break")
-			break
-		}
-		fmt.Println(i)
-	}
+	// for i := 0; i < 10; i++ {
+	// 	if i == 3 {
+	// 		fmt.Println("Continue")
+	// 		continue
+	// 	}
+	// 	if i > 5 {
+	// 		fmt.Println("Break")
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
 
-	sum := 1
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
+	// sum := 1
+	// for sum < 10 {
+	// 	sum += sum
+	// 	fmt.Println(sum)
+	// }
+
+	for {
+		fmt.Println("Hello")
 	}
 }
