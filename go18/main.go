957a9b3354b8d04c5792b09b8945ea9e6d151715
Liberbell <liberbell@gmail.com>
2022-06-23 08:29:51 +0900

    main2

--- go18/main.go
+++ go18/main.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	os := "mac"
+
+	switch os {
+	case "mac":
+		fmt.Println("Mac!")
+	case "windows":
+		fmt.Println("Windows!")
+	default:
+		fmt.Println("I don`t know.")
+	}
 
 }
