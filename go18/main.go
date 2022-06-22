f58f711baa63235bca388eee62d27b0deb0013b2
Liberbell <liberbell@gmail.com>
2022-06-23 08:35:55 +0900

    main6

--- go18/main.go
+++ go18/main.go
@@ -11,16 +11,20 @@ func getOsName() string {
 
 func main() {
 
-	switch os := getOsName(); os {
-	case "mac":
-		fmt.Println("Mac!")
-	case "windows":
-		fmt.Println("Windows!")
-	default:
-		fmt.Println("I don`t know.", os)
-	}
+	// switch os := getOsName(); os {
+	// case "mac":
+	// 	fmt.Println("Mac!")
+	// case "windows":
+	// 	fmt.Println("Windows!")
+	// default:
+	// 	fmt.Println("I don`t know.", os)
+	// }
 
 	t := time.Now()
 	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Morning.")
+	}
 
 }
