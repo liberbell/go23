3df7696cc56c54cd040591229315b26507ad647f
Liberbell <liberbell@gmail.com>
2022-06-23 08:22:32 +0900

    main2

--- go17/main.go
+++ go17/main.go
@@ -6,6 +6,10 @@ func main() {
 	l := []string{"python", "go", "java"}
 
 	for i := 0; i < len(l); i++ {
-		fmt.Println(l[i])
+		fmt.Println(i, l[i])
+	}
+
+	for i, v := range l {
+		fmt.Println(i, v)
 	}
 }
