d140a022223a64736826bf0eddbff929a47c05fa
Liberbell <liberbell@gmail.com>
2022-06-24 22:05:26 +0900

    main9

--- go20/main.go
+++ go20/main.go
@@ -10,6 +10,8 @@ import (
 func loggingSetting(logFile string) {
 	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
 }
 
 func main() {
