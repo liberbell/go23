17c010e9ecf8de5803c2dd9208607633d170cb2b
Liberbell <liberbell@gmail.com>
2022-06-25 18:17:20 +0900

    main2

--- go22/main.go
+++ go22/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func thirdPartyConnectDB() {
 	panic("Unable to connect DB.")
+}
+
+func save() {
+	thirdPartyConnectDB()
+}
+
+func main() {
+	save()
+	fmt.Println("OK?")
 }
