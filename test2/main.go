128109df1dc406552236e20e0fd217aaa6a47621
Liberbell <liberbell@gmail.com>
2022-06-25 18:30:29 +0900

    main3

--- test2/main.go
+++ test2/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
 	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
 	i := 0
-	fmt.Println(l[i])
+	for _, value := range l {
+		switch l[i] > value {
+		case condition:
+
+		}
+	}
 	// for _, value1 := range l {
 	// 	num1 := value1
 
