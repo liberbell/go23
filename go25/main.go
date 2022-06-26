1aa6372d101cf095d6b455eb0908678e301de1df
Liberbell <liberbell@gmail.com>
2022-06-27 08:41:47 +0900

    main5

--- go25/main.go
+++ go25/main.go
@@ -17,6 +17,9 @@ func main() {
 	s := make([]int, 0)
 	fmt.Printf("%T\n", s)
 
-	m := make(map[string])
+	m := make(map[string]int)
 	fmt.Printf("%T\n", m)
+
+	var p *int = new(int)
+	fmt.Printf("%T\n", p)
 }
