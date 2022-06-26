57276d3ddb8077f30fa1f7480605a21c778d7638
Liberbell <liberbell@gmail.com>
2022-06-26 09:42:14 +0900

    main14

--- test2/main.go
+++ test2/main.go
@@ -1,42 +1,44 @@
 package main
 
+import "fmt"
+
 func main() {
 	// l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
 	// i := 0
 	// result := 0
-	for index, value := range l {
-		// fmt.Println(l[i])
-		// fmt.Println(index, value)
-		// if index == 0 {
-		// 	result = value
-		// } else if result > value {
-		// 	fmt.Println("p1:", value)
-		// 	result = value
-		// }
-		// 	if l[i] >= value {
-		// 		fmt.Println(l[i], value)
-		// 		result = value
-		// 		fmt.Println("Value:", result)
-		// 	}
-		// 	fmt.Println("lengs: ", len(l))
-		// 	fmt.Println("index: ", i)
-		// 	if i < len(l) {
-		// 		i = i + 1
-		// 		// fmt.Println(l[i])
-		// 	} else {
+	// for index, value := range l {
+	// fmt.Println(l[i])
+	// fmt.Println(index, value)
+	// if index == 0 {
+	// 	result = value
+	// } else if result > value {
+	// 	fmt.Println("p1:", value)
+	// 	result = value
+	// }
+	// 	if l[i] >= value {
+	// 		fmt.Println(l[i], value)
+	// 		result = value
+	// 		fmt.Println("Value:", result)
+	// 	}
+	// 	fmt.Println("lengs: ", len(l))
+	// 	fmt.Println("index: ", i)
+	// 	if i < len(l) {
+	// 		i = i + 1
+	// 		// fmt.Println(l[i])
+	// 	} else {
 
-		// 	}
-		// 	fmt.Println("Result: ", result)
-		// }
-		// for _, value1 := range l {
-		// 	num1 := value1
+	// 	}
+	// 	fmt.Println("Result: ", result)
+	// }
+	// for _, value1 := range l {
+	// 	num1 := value1
 
-		// 	switch value1 >  {
-		// 	case condition:
+	// 	switch value1 >  {
+	// 	case condition:
 
-		// 	}
-		// }
-	}
+	// 	}
+	// }
+	// }
 	// fmt.Println("Result:", result)
 
 	m := map[string]int{
@@ -46,6 +48,10 @@ func main() {
 		"orange": 80,
 		"papaya": 500,
 		"kiwi":   90,
+	}
+
+	for index, value := range m {
+		fmt.Println(index, value)
 	}
 
 }
