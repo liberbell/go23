330d7e6961f171e54039b99b387e9c3937da582d
Liberbell <liberbell@gmail.com>
2022-06-26 09:40:44 +0900

    main13

--- test2/main.go
+++ test2/main.go
@@ -1,20 +1,18 @@
 package main
 
-import "fmt"
-
 func main() {
-	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+	// l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
 	// i := 0
-	result := 0
+	// result := 0
 	for index, value := range l {
 		// fmt.Println(l[i])
-		fmt.Println(index, value)
-		if index == 0 {
-			result = value
-		} else if result > value {
-			fmt.Println("p1:", value)
-			result = value
-		}
+		// fmt.Println(index, value)
+		// if index == 0 {
+		// 	result = value
+		// } else if result > value {
+		// 	fmt.Println("p1:", value)
+		// 	result = value
+		// }
 		// 	if l[i] >= value {
 		// 		fmt.Println(l[i], value)
 		// 		result = value
@@ -39,5 +37,15 @@ func main() {
 		// 	}
 		// }
 	}
-	fmt.Println("Result:", result)
+	// fmt.Println("Result:", result)
+
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+
 }
