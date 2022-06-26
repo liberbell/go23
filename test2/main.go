1c8498a4b29fbd38717b3acbd9779b2126337498
Liberbell <liberbell@gmail.com>
2022-06-26 09:24:05 +0900

    main6

--- test2/main.go
+++ test2/main.go
@@ -4,27 +4,37 @@ import "fmt"
 
 func main() {
 	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
-	i := 0
-	for _, value := range l {
+	// i := 0
+	result := 0
+	for index, value := range l {
 		// fmt.Println(l[i])
-		if l[i] >= value {
-			fmt.Println(l[i], value)
-			result := value
-			fmt.Println("Value:", result)
+		fmt.Println(index, value)
+		if index == 0 {
+			result = value
 		}
-		if i < len(l) {
-			i = i + 1
-			// fmt.Println(l[i])
-		} else {
-			fmt.Println("Result: ", l[i])
-		}
+		fmt.Println("Result:", result)
+		// 	if l[i] >= value {
+		// 		fmt.Println(l[i], value)
+		// 		result = value
+		// 		fmt.Println("Value:", result)
+		// 	}
+		// 	fmt.Println("lengs: ", len(l))
+		// 	fmt.Println("index: ", i)
+		// 	if i < len(l) {
+		// 		i = i + 1
+		// 		// fmt.Println(l[i])
+		// 	} else {
+
+		// 	}
+		// 	fmt.Println("Result: ", result)
+		// }
+		// for _, value1 := range l {
+		// 	num1 := value1
+
+		// 	switch value1 >  {
+		// 	case condition:
+
+		// 	}
+		// }
 	}
-	// for _, value1 := range l {
-	// 	num1 := value1
-
-	// 	switch value1 >  {
-	// 	case condition:
-
-	// 	}
-	// }
 }
