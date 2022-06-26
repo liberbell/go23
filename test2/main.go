d39d3567418461e5336fb3ad3509e24a02ec43d6
Liberbell <liberbell@gmail.com>
2022-06-26 09:10:38 +0900

    main1

--- test2/main.go
+++ test2/main.go
@@ -4,9 +4,10 @@ func main() {
 	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
 	i := 0
 	for _, value := range l {
-		switch l[i] {
+		switch {
 		case l[i] > value:
-
+			result := value
+			i = i + 1
 		}
 	}
 	// for _, value1 := range l {
