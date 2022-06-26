b95eabe0e883743eff708e3c6bdcc3f98eda9bce
Liberbell <liberbell@gmail.com>
2022-06-27 08:31:40 +0900

    main4

--- go24/main.go
+++ go24/main.go
@@ -2,13 +2,20 @@ package main
 
 import "fmt"
 
+func one(x int) {
+	x = 1
+}
+
 func main() {
 	var n int = 100
+	// fmt.Println(n)
+	// fmt.Println(&n)
+
+	// var p *int = &n
+
+	// fmt.Println(p)
+	// fmt.Println(*p)
+
+	one(n)
 	fmt.Println(n)
-	fmt.Println(&n)
-
-	var p *int = &n
-
-	fmt.Println(p)
-	fmt.Println(*p)
 }
