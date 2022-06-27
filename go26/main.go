b97824c0edd7c165550add1641f77e6531bf14da
Liberbell <liberbell@gmail.com>
2022-06-28 08:39:53 +0900

    main11

--- go26/main.go
+++ go26/main.go
@@ -1,42 +1,49 @@
 package main
 
-import "fmt"
-
 type Vertex struct {
 	X int
 	Y int
 	S string
 }
 
-func main() {
-	v := Vertex{X: 10, Y: 20}
-	fmt.Println(v)
-	fmt.Println(v.X, v.Y)
-
+func changeVertex(v Vertex) {
 	v.X = 1000
-	fmt.Println(v)
+}
 
-	v2 := Vertex{X: 200}
-	fmt.Println(v2)
+func changeVertex2(v *Vertex) {
+	v.X = 1000
+}
 
-	v3 := Vertex{4, 8, "test"}
-	fmt.Println(v3)
+func main() {
+	// v := Vertex{X: 10, Y: 20}
+	// fmt.Println(v)
+	// fmt.Println(v.X, v.Y)
 
-	v4 := Vertex{}
-	fmt.Println(v4)
-	fmt.Printf("%T\n", v4)
+	// v.X = 1000
+	// fmt.Println(v)
 
-	var v5 Vertex
-	fmt.Println(v5)
-	fmt.Printf("%T\n", v5)
+	// v2 := Vertex{X: 200}
+	// fmt.Println(v2)
 
-	v6 := new(Vertex)
-	fmt.Println(v6)
-	fmt.Printf("%T\n", v6)
+	// v3 := Vertex{4, 8, "test"}
+	// fmt.Println(v3)
 
-	v7 := &Vertex{}
-	fmt.Printf("%T %v\n", v7, v7)
+	// v4 := Vertex{}
+	// fmt.Println(v4)
+	// fmt.Printf("%T\n", v4)
 
-	s := []int{}
-	fmt.Println(s)
+	// var v5 Vertex
+	// fmt.Println(v5)
+	// fmt.Printf("%T\n", v5)
+
+	// v6 := new(Vertex)
+	// fmt.Println(v6)
+	// fmt.Printf("%T\n", v6)
+
+	// v7 := &Vertex{}
+	// fmt.Printf("%T %v\n", v7, v7)
+
+	// s := []int{}
+	// fmt.Println(s)
+
 }
