49889d1b9fafec2a20d46f40c0a5d3a1b5073506
Liberbell <liberbell@gmail.com>
2022-06-29 08:40:41 +0900

    main5

--- test3/main.go
+++ test3/main.go
@@ -1,13 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
-	var i int = 10
-	var p *int
+	// var i int = 10
+	// var p *int
 
-	p = &i
-	var j int
-	j = *p
-	fmt.Println(j)
+	// p = &i
+	// var j int
+	// j = *p
+	// fmt.Println(j)
+
+	var i int = 100
+	var j int = 200
 }
