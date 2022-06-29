9d5b281bfc10b307692618d70ba3f31ab43cb9a1
Liberbell <liberbell@gmail.com>
2022-06-30 08:48:30 +0900

    main5

--- go29/main.go
+++ go29/main.go
@@ -10,12 +10,12 @@ type Person struct {
 	Name string
 }
 
-func (p Person) Say() {
+func (p *Person) Say() {
 	p.Name = "Mr." + p.Name
 	fmt.Println(p.Name)
 }
 
 func main() {
-	var mike Human = Person{"Mile"}
+	var mike Human = &Person{"Mike"}
 	mike.Say()
 }
