2faed4cdcf1a647cd0fe58c3c38213bd0c9bf877
Liberbell <liberbell@gmail.com>
2022-07-01 08:47:30 +0900

    main6

--- go30/main.go
+++ go30/main.go
@@ -8,11 +8,19 @@ func do(i interface{}) {
 	// fmt.Println(ii)
 	ss := i.(string)
 	fmt.Println(ss + "!")
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	case bool:
+		fmt.Println(v)
+	}
 }
 
 func main() {
 	// var i interface{} = 10
-	// do(i)
+	do(10)
 	do("Mike")
-	// do(true)
+	do(true)
 }
