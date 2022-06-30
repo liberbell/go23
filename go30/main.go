232baa98cff6b23cd1176ae5c41a1e9040d16323
Liberbell <liberbell@gmail.com>
2022-07-01 08:44:56 +0900

    main4

--- go30/main.go
+++ go30/main.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func do(i interface{}) {
-	ii := i.(int)
-	ii *= 2
-	fmt.Println(ii)
+	// ii := i.(int)
+	// ii *= 2
+	// fmt.Println(ii)
+	ss := i.(string)
+	fmt.Println(ss)
 }
 
 func main() {
-	var i interface{} = 10
-	do(i)
-	// do("Mike")
+	// var i interface{} = 10
+	// do(i)
+	do("Mike")
 	// do(true)
 }
