86934a3ed6e2c3be65ccb725751401a92e1c9771
Liberbell <liberbell@gmail.com>
2022-07-02 10:53:16 +0900

    main11

--- go32/main.go
+++ go32/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"strings"
 )
 
@@ -35,5 +36,9 @@ func main() {
 	for {
 		n, err := r.Read(b)
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
 	}
 }
