2a9cdab2c259a302ed8416b769972ecf91691bcf
Liberbell <liberbell@gmail.com>
2022-07-03 14:22:03 +0900

    main2

--- go32/main.go
+++ go32/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"strings"
 )
 
@@ -33,11 +33,13 @@ func main() {
 	r := strings.NewReader("Hello, Reader!")
 
 	b := make([]byte, 8)
+	e := errors.New("EOF")
 	for {
 		n, err := r.Read(b)
 		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
 		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
+		// if err == io.EOF {
+		if err == e {
 			break
 		}
 	}
