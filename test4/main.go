13dd9ea11062aec0aea41b6f20f406bcb1794778
Liberbell <liberbell@gmail.com>
2022-07-03 14:35:14 +0900

    main3

--- test4/main.go
+++ test4/main.go
@@ -12,9 +12,14 @@ func (r Vertex) Plus() int {
 	return r.X + r.Y
 }
 
+func (v Vertex) String() string {
+	return fmt.Sprintf("X is %v! Y is %v!\n", e.X, e.Y)
+}
+
 func main() {
 	// v := Vertex{3, 4}
 	// fmt.Println(v.Plus())
 	v := Vertex{3, 4}
 	fmt.Println(v)
+	// e1 := &ValuePrint{3, 4}
 }
