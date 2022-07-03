c01b24891945909800233445eeb16240bd604bd0
Liberbell <liberbell@gmail.com>
2022-07-03 14:41:20 +0900

    main4

--- go33/main.go
+++ go33/main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func goroutine(s string) {
 	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
+		// time.Sleep(100 * time.Millisecond)
 		fmt.Println(s)
 	}
 }
 
 func normal(s string) {
 	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
+		// time.Sleep(100 * time.Millisecond)
 		fmt.Println(s)
 	}
 }
