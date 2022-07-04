408546823d0fb388abb575d28c113de9e32d5187
Liberbell <liberbell@gmail.com>
2022-07-05 08:50:17 +0900

    main3

--- go37/main.go
+++ go37/main.go
@@ -1,7 +1,15 @@
 package main
 
+func producer(first chan int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
+}
+
 func main() {
 	first := make(chan int)
 	second := make(chan int)
 	third := make(chan int)
+
 }
