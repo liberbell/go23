645d2cbf6f775caa2a186314b1bfef9b7a1b54e1
Liberbell <liberbell@gmail.com>
2022-07-04 17:13:45 +0900

    main5

--- go34/main.go
+++ go34/main.go
@@ -10,10 +10,20 @@ func goroutine1(s []int, c chan int) {
 	c <- sum
 }
 
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
 func main() {
 	s := []int{1, 2, 3, 4, 5}
 	c := make(chan int)
 	go goroutine1(s, c)
 	x := <-c
 	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
 }
