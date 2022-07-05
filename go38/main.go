dd2758ce10b89efa2250dccc9c260b37fc26f539
Liberbell <liberbell@gmail.com>
2022-07-06 08:40:17 +0900

    main4

--- go38/main.go
+++ go38/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func goroutine(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
@@ -11,13 +9,15 @@ func goroutine(s []int, c chan int) {
 }
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int)
-	go goroutine(s, c)
-	go goroutine(s, c)
+	// s := []int{1, 2, 3, 4, 5}
+	// c := make(chan int)
+	// go goroutine(s, c)
+	// go goroutine(s, c)
 
-	x := <-c
-	fmt.Println(x)
-	y := <-c
-	fmt.Println(y)
+	// x := <-c
+	// fmt.Println(x)
+	// y := <-c
+	// fmt.Println(y)
+	c1 := make(chan string)
+	c2 := make(chan string)
 }
