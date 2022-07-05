d0c30fa70b467b14a326cabd27b88782acf5774e
Liberbell <liberbell@gmail.com>
2022-07-06 08:35:36 +0900

    main2

--- go38/main.go
+++ go38/main.go
@@ -5,4 +5,12 @@ func goroutine(s []int, c chan int) {
 	for _, v := range s {
 		sum += v
 	}
+	c <- sum
+}
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int)
+	go goroutine(s, c)
+	go goroutine(s, c)
 }
