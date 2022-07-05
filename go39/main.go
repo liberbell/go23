68834cc03e63136c3c2143fd117f4f6201048b67
Liberbell <liberbell@gmail.com>
2022-07-06 08:55:36 +0900

    main5

--- go39/main.go
+++ go39/main.go
@@ -8,13 +8,15 @@ import (
 func main() {
 	tick := time.Tick(100 * time.Millisecond)
 	boom := time.After(500 * time.Millisecond)
+OuterLoop:
 	for {
 		select {
 		case t := <-tick:
 			fmt.Println("Tick!", t)
 		case <-boom:
 			fmt.Println("Boom!")
-			return
+			break OuterLoop
+			// return
 		default:
 			fmt.Println("   .")
 			time.Sleep(50 * time.Millisecond)
