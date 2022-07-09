82a43fe23011716cdb021f54fe39bea3d47b64ab
Liberbell <liberbell@gmail.com>
2022-07-09 15:10:32 +0900

    main3

--- go43/main.go
+++ go43/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"regexp"
 )
 
 func main() {
-	t := time.Now()
-	fmt.Println(t)
-	fmt.Println(t.Format(time.RFC3339))
-	fmt.Println(t.Year())
-	fmt.Println(t.Month())
-	fmt.Println(t.Hour())
+	// t := time.Now()
+	// fmt.Println(t)
+	// fmt.Println(t.Format(time.RFC3339))
+	// fmt.Println(t.Year())
+	// fmt.Println(t.Month())
+	// fmt.Println(t.Hour())
+	match, _ := regexp.MatchString("a([a-z]+)e", "apple")
+	fmt.Println(match)
 }
