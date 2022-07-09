6f2a82f60df1bdddab7d9fc7fa82bb9a84a6dfc4
Liberbell <liberbell@gmail.com>
2022-07-09 14:55:58 +0900

    main2

--- go42/main.go
+++ go42/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	spy, _ := quote.NewQuoteFromYahoo("spy", "2016-01-01", "2016-04-01", quote.Daily, true)
+	spy, _ := quote.NewQuoteFromYahoo("spy", "2022-01-01", "2022-04-01", quote.Daily, true)
 	fmt.Print(spy.CSV())
 	rsi2 := talib.Rsi(spy.Close, 2)
 	fmt.Println(rsi2)
