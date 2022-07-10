e5404bc84be1210abfe1228616211b4de65d33ab
Liberbell <liberbell@gmail.com>
2022-07-10 11:22:30 +0900

    main2

--- go44/main.go
+++ go44/main.go
@@ -3,4 +3,13 @@ package main
 func main() {
 	i := []int{5, 3, 2, 8, 7}
 	s := []string{"d", "a", "f"}
+	p := []string {
+		Name string
+		Age int
+	} {
+		{"Bob":43}
+		{"Eric":73}
+		{"Alex"34}
+		{"Elton": 69}
+	}
 }
