2b32c028cd24790f2114987bcd1bd098bdbe92f0
Liberbell <liberbell@gmail.com>
2022-07-10 11:23:13 +0900

    main3

--- go44/main.go
+++ go44/main.go
@@ -3,13 +3,14 @@ package main
 func main() {
 	i := []int{5, 3, 2, 8, 7}
 	s := []string{"d", "a", "f"}
-	p := []string {
+	p := []struct {
 		Name string
-		Age int
-	} {
-		{"Bob":43}
-		{"Eric":73}
-		{"Alex"34}
-		{"Elton": 69}
+		Age  int
+	}{
+		{"Bob": 43},
+		{"Eric": 73},
+		{"Alex": 34},
+		{"Elton": 69},
 	}
+
 }
