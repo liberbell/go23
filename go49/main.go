35a47b30b083730b32a8ec0ed2a291930998b6ec
Liberbell <liberbell@gmail.com>
2022-07-12 08:58:29 +0900

    main4

--- go49/main.go
+++ go49/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Person struct {
 	Name      string
 	Age       int
@@ -8,5 +13,9 @@ type Person struct {
 
 func main() {
 	b := []byte(`{"name":"Elton", "Age":72, "nicknames": ["a", "b", "c"]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
 
 }
