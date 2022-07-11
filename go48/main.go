8a53cbb485ce7cce53acd8b36a06e8b30806a91c
Liberbell <liberbell@gmail.com>
2022-07-12 08:38:36 +0900

    main4

--- go48/main.go
+++ go48/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, _ := http.Get("https://google.com")
-	defer resp.Body.Close()
+	// resp, _ := http.Get("https://google.com")
+	// defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+	base, err := url.Parse("https://goog le.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+	reference, _ := url.Parse("/")
+	fmt.Println(base)
 }
