7d509547dd60ecc9e1ceed6e2ba4097ff98675d3
Liberbell <liberbell@gmail.com>
2022-07-12 08:57:32 +0900

    main3

--- go49/main.go
+++ go49/main.go
@@ -1,5 +1,12 @@
 package main
 
+type Person struct {
+	Name      string
+	Age       int
+	nicknames []string
+}
+
 func main() {
-	b := []byte(`{"name":"Elton", "Age":72, "nickname": "elton"}`)
+	b := []byte(`{"name":"Elton", "Age":72, "nicknames": ["a", "b", "c"]}`)
+
 }
