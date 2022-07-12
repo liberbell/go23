31e586091de8fc12fc898cba9083737b2b044ab5
Liberbell <liberbell@gmail.com>
2022-07-13 08:55:46 +0900

    main3

--- go50/main.go
+++ go50/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"fmt"
+)
+
 func main() {
 	const apiKey = "User1Key"
 	const apiSecret = "User1Secret"
+
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	fmt.Println(h)
 }
