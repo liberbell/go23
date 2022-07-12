64d066f2b9e3d42391da21c33bf2d1f0ee7c67ce
Liberbell <liberbell@gmail.com>
2022-07-13 08:38:48 +0900

    main5

--- go49/main.go
+++ go49/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 type Person struct {
-	Name      string   `json:"name"`
-	Age       int      `json:"age,string"`
+	Name      string   `json:"name,omitempty"`
+	Age       int      `json:"age,omitempty"`
 	Nicknames []string `json:"nicknames"`
 }
 
 func main() {
-	b := []byte(`{"name":"Elton", "age":"72", "nicknames": ["a", "b", "c"]}`)
+	b := []byte(`{"name":"Elton", "age":20, "nicknames": ["a", "b", "c"]}`)
 	var p Person
 	if err := json.Unmarshal(b, &p); err != nil {
 		fmt.Println(err)
