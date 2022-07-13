709e899ef5b27a1b36879d0b92fa24ff9d767802
Liberbell <liberbell@gmail.com>
2022-07-14 08:44:28 +0900

    main4

--- go51/main.go
+++ go51/main.go
@@ -8,14 +8,18 @@ import (
 	"golang.org/x/sync/semaphore"
 )
 
-var s *semaphore.Weighted = semaphore.NewWeighted(1)
+var s *semaphore.Weighted = semaphore.NewWeighted(2)
 
 func lognProcess(ctx context.Context) {
-	if err := s.Acquire(ctx, 1); err != nil {
-		fmt.Println(err)
-		return
+	// if err := s.Acquire(ctx, 1); err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer s.Release(1)
+	isAcquire := s.TryAcquire(1)
+	if isAcquire {
+		fmt.Println("Could not get lock")
 	}
-	defer s.Release(1)
 	fmt.Println("Wait...")
 	time.Sleep(1 * time.Second)
 	fmt.Println("Done.")
