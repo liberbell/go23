735432179839b300dca43e20ca09a5f01bf9613d
Liberbell <liberbell@gmail.com>
2022-07-15 08:51:00 +0900

    main2

--- go53/main.go
+++ go53/main.go
@@ -1,7 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/markcheno/go-quote"
+)
+
 func main() {
 	spy, _ := quoto.NewQuotoFromYahoo(
 		"spy", "2022-07-01", "2022-07-14", quote.Daily, true,
 	)
+	fmt.Print(spy.CSV())
 }
