6fdde8bf84b2368784f982339427c8d16ae1fff7
Liberbell <liberbell@gmail.com>
2022-07-15 08:38:47 +0900

    main4

--- go52/main.go
+++ go52/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "gopkg.in/go-ini/ini.v1"
+import (
+	"fmt"
+
+	"gopkg.in/go-ini/ini.v1"
+)
 
 type Configlist struct {
 	Port      int
@@ -15,6 +19,12 @@ func init() {
 	Config = Configlist{
 		Port:      cfg.Section("web").Key("port").MustInt(),
 		DBname:    cfg.Section("db").Key("name").MustString("example.sql"),
-		SQLDriver: cfg.Section("db").Key("driver").MustString("sqlite3"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
 	}
+}
+
+func main() {
+	fmt.Printf("%T %v\n", Config.Port, Config.Port)
+	fmt.Printf("%T %v\n", Config.DBname, Config.DBname)
+	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
 }
