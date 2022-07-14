974c16593b90b9247a7fa10407eb15caa3679a97
Liberbell <liberbell@gmail.com>
2022-07-15 08:53:14 +0900

    main4

--- go53/main.go
+++ go53/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	spy, _ := quoto.NewQuotoFromYahoo(
+	spy, _ := quote.NewQuoteFromYahoo(
 		"spy", "2022-07-01", "2022-07-14", quote.Daily, true,
 	)
 	fmt.Print(spy.CSV())
