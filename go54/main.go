d6f786f84e94e0a2210bb323da2b0836c370928c
Liberbell <liberbell@gmail.com>
2022-07-15 09:05:39 +0900

    main1

---
+++ go54/main.go
@@ -0,0 +1,9 @@
+package main
+
+type JsonRPC2 struct {
+	Version string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Result  interface{} `json:"result,omitempty"`
+	Id      *int        `json:"id,omitempty"`
+}
