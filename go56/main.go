87af37f1f6bfd3a4652e778ed5b801aef17fb398
Liberbell <liberbell@gmail.com>
2022-07-19 13:41:05 +0900

    main4

--- go56/main.go
+++ go56/main.go
@@ -33,7 +33,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	//
 	renderTemplate(w, "view", p)
 }
@@ -54,6 +58,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	err := p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
