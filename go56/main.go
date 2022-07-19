0120f74b5b42f307fa5ab3c6843ba86e44e45a20
Liberbell <liberbell@gmail.com>
2022-07-19 13:39:41 +0900

    main4

--- go56/main.go
+++ go56/main.go
@@ -52,6 +52,10 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
 func main() {
