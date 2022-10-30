5f4895a2e9193eee9a5480404cdfb66c66bb618f
Liberbell <liberbell@gmail.com>
2022-10-30 21:51:47 +0900

    employeesData2

--- go58/employeesData.sql
+++ go58/employeesData.sql
@@ -1,3 +1,15 @@
 USE 'company';
 
 INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'fredericton', 'sales');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'boston', 'development');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'toronto', 'development');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'new york', 'development');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'houston', 'sales');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'tampa', 'operations');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'vancouver', 'operations');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'fredericton', 'sales');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'fredericton', 'development');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'fredericton', 'sales');
+INSERT INTO 'company'.'employees' ('name', 'city', 'dept') VALUES ('joe', 'fredericton', 'development');
+
+SELECT * FROM 'employeees;
