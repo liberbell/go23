bfc564f165c14d86d00ba8481ae8e3ab5961df94
Liberbell <liberbell@gmail.com>
2022-10-30 21:59:45 +0900

    sqlschima5

--- go58/sqlschima.sql
+++ go58/sqlschima.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE 'company';
+CREATE DATABASE `company`;
 
-USE 'company';
+USE `company`;
 
 CREATE TABLE IF NOT EXISTS 'employees' (
-    'id' INT unsigned NOT NULL AUTO_INCREMENT,
-    'name' VARCHAR(20),
-    'city' VARCHAR(20) DEFAULT 'fredericton',
-    'dept' VARCHAR(20) DEFAULT 'onbording',
-    PRIMARY KEY ('id')
+    `id` INT unsigned NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(20),
+    `city` VARCHAR(20) DEFAULT 'fredericton',
+    `dept` VARCHAR(20) DEFAULT 'onbording',
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4;
