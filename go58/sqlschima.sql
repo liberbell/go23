8e464c98b853f8fbcb736e10c5e82598291e03cd
Liberbell <liberbell@gmail.com>
2022-10-30 22:01:20 +0900

    sqlschima6

--- go58/sqlschima.sql
+++ go58/sqlschima.sql
@@ -5,7 +5,7 @@ USE `company`;
 CREATE TABLE IF NOT EXISTS `employees` (
     `id` INT unsigned NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20),
-    `city` VARCHAR(20) DEFAULT `fredericton`,
-    `dept` VARCHAR(20) DEFAULT `onbording`,
+    `city` VARCHAR(20) DEFAULT "fredericton",
+    `dept` VARCHAR(20) DEFAULT "onbording",
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8mb4;
