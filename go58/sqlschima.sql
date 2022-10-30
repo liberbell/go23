2379e1c29764d8ae7889d6e1c647dd955f7beb26
Liberbell <liberbell@gmail.com>
2022-10-30 09:55:11 +0900

    sqlschima3

--- go58/sqlschima.sql
+++ go58/sqlschima.sql
@@ -5,4 +5,5 @@ USE 'company';
 CREATE TABLE IF NOT EXISTS 'employee' (
     'id' INT unsigned NOT NULL AUTO_INCREMENT,
     'name' VARCHAR(20),
-    'city' VARCHAR(20), DEFAULT 'fredericton',
+    'city' VARCHAR(20) DEFAULT 'fredericton',
+    'dept' VARCHAR(20) DEFAULT 'onbording',
